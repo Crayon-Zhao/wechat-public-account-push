15735e55d6582c788cc7c36312334f43e1aee471
Crayon-Zhao <54010157+Crayon-Zhao@users.noreply.github.com>
2022-10-13 10:18:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,11 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1998', date: '08-27',
+          type: '*生日', name: '晴宝农历', year: '1998', date: '08-27',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1998', date: '10-17',
+          type: '生日', name: '晴宝', year: '1998', date: '10-17',
         },
         {
           type: '节日', name: '相识纪念日', year: '2024', date: '01-02',
