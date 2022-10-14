6755e452efb8c5d6435f13d3b69275b1d342fa45
Crayon-Zhao <54010157+Crayon-Zhao@users.noreply.github.com>
2022-10-14 14:25:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -136,6 +136,7 @@ const USER_CONFIG = {
       customizedDateList: [],
       courseSchedule: null
     }
+  ],
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'BaEBNImTLldhC-7natT0skdOdtQwLzbjFAbISLzoAi0',
