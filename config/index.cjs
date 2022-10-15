3781cc6eb5f4b084b13bbb1dad96026e77f753bc
Crayon-Zhao <54010157+Crayon-Zhao@users.noreply.github.com>
2022-10-15 08:33:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,7 +31,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oFfAE6a21PdddMlx98330TaTLcVQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'q2fCnea0cPixNoBlp2OYJre0IFgQivAIdJxqcVLyt_M',
+      useTemplateId: 'N-BwYqytqMcFl9Hr2yrIb2rD8-hXn4N6FMmOoqk2fAc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-17',
       festivals: [
